2efe950d21f64251676240d3abd7e44532dac9d7
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-25 19:00:04 +0630

    Async await function to make API request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  console.log("this message is going to load everytime the component renders");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://book-club-json.herokuapp.com/books"
+        );
+        console.log(`here's what our fetch request returns`, response);
+
+        if (response.status === 200) {
+          const books = await response.json();
+          console.log(`our json object`, books);
+          setBooks(books);
+        }
+      } catch (errors) {
+        console.log(`The errors`, errors);
+      }
+
+      // async allows us to not hand-code promise chains ().then().then()
+    };
+    fetchData();
+  }, []);
+
+  console.log(`the books array in our state: `, books);
+  return <div>Hello World</div>;
+};
 
 export default App;
